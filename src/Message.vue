b1e3534f981198238168f6295299a1fb92ddb3d6
Ruli Helmi <rulinizar@gmail.com>
2021-01-16 17:04:06 +0700

    init

---
+++ src/Message.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This is grate message</h1>
+        <app-input></app-input>
+    </div>
+</template>
+
+<script>
+import input from './input.vue';
+export default {
+    components: {
+        'app-input': input
+    }
+}
+</script>
